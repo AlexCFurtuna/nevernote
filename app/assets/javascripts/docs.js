d410a5326d7835c9a3e1143851661ad19a98d04c
Alex <alexcfurtuna@gmail.com>
2018-07-30 10:19:52 +0100

    fixing jquery issues

--- app/assets/javascripts/docs.js
+++ app/assets/javascripts/docs.js
@@ -1,8 +1,8 @@
 $(function() {
  $('#trigger').click(function() {
-  $(".section-two").show(1500);
-  $(".meetings").show(1500);
-  $(".wrapper_with_padding").hide(750);
-  $(".section-two.two").hide(750);
+  $("#section-two").show(1500);
+  $("#meetings").show(1500);
+  $("#wrapper_w_padding").hide(750);
+  $("#toHide").hide(750);
   });
 });
